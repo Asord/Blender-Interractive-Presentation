6ad6a0a2e6cfc041420ae10a3dca0fba023fe483
ValentinRiu <val2009.34@gmail.Com>
2018-05-16 11:42:56 +0200

    Définition de la propriété pour la slide active.

--- scripts/Interface/Props.py
+++ scripts/Interface/Props.py
@@ -1,4 +1,5 @@
 from bpy import types, props
+from Slides import GestionSlides
 
 #abstract property skeleton
 class Property(types.PropertyGroup):
@@ -7,8 +8,44 @@ class Property(types.PropertyGroup):
 
 class PropNumberSlide(Property):
 
-    size = props.IntProperty(
-            name = "size",
+    gestSlide = GestionSlides()
+    nbSlide = 0
+
+
+
+    def updateProp(self, context):
+        """S'exécute quand la valeur change"""
+        self.nbSlide = self.gestSlide.getNbSlides()
+        print(self.Active_Slide)
+        print('UpdateProp')
+        if self.Active_Slide > self.nbSlide:
+            self.Active_Slide = self.Active_Slide - 1
+            self.gestSlide.setActiveSlide(self.Active_Slide)
+
+    """def setProp(self, value):
+        PropNumberSlide.nbSlide = PropNumberSlide.gestSlide.getNbSlides()
+        print("Valeur : ")
+        print(value)
+        print("Slide : ")
+        print(PropNumberSlide.nbSlide)
+        if 0 <= value < PropNumberSlide.nbSlide:
+            PropNumberSlide.Active_Slide = value
+        else :
+            return None
+
+        print(PropNumberSlide.nbSlide)"""
+
+    Active_Slide = props.IntProperty(
+            name = "Active slide",
             description = "size of the object to add",
-            default = 1, max=10, min=0
+            default = 0, max = 10, min=0, update = updateProp
     )
+
+
+
+
+
+
+
+
+
