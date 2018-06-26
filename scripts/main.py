0aabbd130039fb714357e224a2a4c558aa8e75ec
Killian <killian.perche@outlook.fr>
2018-06-26 20:49:11 +0200

    Minor improvement

--- scripts/main.py
+++ scripts/main.py
@@ -6,51 +6,54 @@ from bpy import utils
 from Interface import *
 from Managers.SlidesManager import SlidesManager
 
-classes = (
-    PropNumberSlide,
+class main:
+    classes = (
+        PropNumberSlide,
 
-    OperatorCameraView,
-    OperatorAddSlide,
-    OperatorRemoveSlide,
-    OperatorAddAnim,
-    OperatorLinkObject,
+        OperatorCameraView,
+        OperatorAddSlide,
+        OperatorRemoveSlide,
+        OperatorAddAnim,
+        OperatorLinkObject,
 
-    PopupDeleteSlide,
+        PopupDeleteSlide,
 
-    MenuMain,
-    MenuSlide,
-    MenuAnimation,
-    MenuObjet,
-)
+        MenuMain,
+        MenuSlide,
+        MenuAnimation,
+        MenuObjet,
+    )
 
-def register():
-    """
-    Register all Blender related classes contained inside the 'classes' container
-    Also register all props from registerProps() contained inside Interface.Props file
-    Create the first instance of SlidesManager singloton
-    """
-    for cl in classes:
+    @staticmethod
+    def register():
+        """
+        Register all Blender related classes contained inside the 'classes' container
+        Also register all props from registerProps() contained inside Interface.Props file
+        Create the first instance of SlidesManager singloton
+        """
+        for cl in main.classes:
 
-        try:
-            utils.register_class(cl)
-        except ValueError:
-            utils.unregister_class(cl)
-            utils.register_class(cl)
+            try:
+                utils.register_class(cl)
+            except ValueError:
+                utils.unregister_class(cl)
+                utils.register_class(cl)
 
-    registerProps()
+        registerProps()
 
-    SlidesManager()
+        SlidesManager()
 
-def unregister():
-    """
-    Unregister all Blender related classes contained inside the 'classes' container
-    """
-    for cl in classes:
-        try:
-            utils.unregister_class(cl)
-        except RuntimeError:
-            pass
+    @staticmethod
+    def unregister():
+        """
+        Unregister all Blender related classes contained inside the 'classes' container
+        """
+        for cl in main.classes:
+            try:
+                utils.unregister_class(cl)
+            except RuntimeError:
+                pass
 
 if __name__ == "__main__":
-    unregister()
-    register()
+    main.unregister()
+    main.register()
