ec4f7de9a7376160af760e1426dcc077f3092184
ValentinRiu <val2009.34@gmail.Com>
2018-06-29 13:06:39 +0200

    - Redéfinition du "addObject" - Redéfinition du "removeObject" - Redéfinition du "addAnimation" - Définition du "renameObject" - Définition du "removeAnimation"

--- scripts/Managers/SlidesManager.py
+++ scripts/Managers/SlidesManager.py
@@ -1,6 +1,6 @@
 from bpy import context, ops, data
 from BPL import Singloton, Debug
-from Managers import AnimationManager
+from Managers.AnimationManager import AnimationManager
 
 
 
@@ -9,49 +9,96 @@ class Slides(object):
     def __init__(self, gestionSlide, slidePos):
         self.gestionSlide = gestionSlide
         self.cameraPos = [slidePos*20,5,0]
-        self.transitionDone = True
-        self.actionDone = True
         self.listObjects = []
         self.listAnimation = []
-        self.pos = 0
+        self.startMotion = [0.0, 0.0, 0.0]
+        self.endMotion = [0.0, 0.0, 0.0]
 
     def addObject(self, obj):
-        self.listObjects.append(obj)
-        self.listAnimation.append(None)
+        if obj.name[0:5]!= "Slide":
+            self.listObjects.append(obj)
+            self.renameObject(obj, self.gestionSlide.posActiveSlide + 1, len(self.listObjects))
+            self.listAnimation.append(None)
+            context.scene.objects.active = obj
+            ops.logic.sensor_add(type = 'MESSAGE')
+            context.scene.objects.active = self.gestionSlide.camera
+            actuators = context.object.game.actuators
+            ops.logic.actuator_add(type = 'MESSAGE')
+            actuator = actuators[-1]
+            actuator.name = obj.name
+            actuator.to_property = obj.name
+            controllers = context.object.game.controllers
+            controller = controllers[-1]
+            controller.link(None, actuator)
+            context.scene.objects.active = obj
+
+    def renameObject(self, obj, nSlide, nObj):
+        if nSlide is None:
+            nSlide = obj.name[5]
+
+        if nObj is None:
+            nObj = obj.name[7]
+
+        obj.name = "Slide{}-{}".format(nSlide, nObj)
 
     def removeObject(self, obj):
+        ops.logic.sensor_remove(sensor=context.active_object.game.sensors[0].name, object=context.active_object.name)
+        controllers = context.object.game.controllers
+        if controllers:
+            if controllers[0].actuators[0].name == "flash":
+                self.removeAnimation("delFlash")
+            elif controllers[0].actuators[0].name == "motion":
+                self.removeAnimation("delMotion")
+        ops.logic.actuator_remove(actuator=obj.name[0:8], object="cam")
+
+
+        for i in range(self.listObjects.index(obj) + 1, len(self.listObjects)):
+            self.renameObject(self.listObjects[i], None, int(self.listObjects[i].name[7]) - 1)
         self.listObjects.remove(obj)
+        obj.name = "Object"
 
     def addAnimation(self, anim):
-        i = 0
         for obj in self.listObjects:
             if obj == context.active_object:
-                self.listAnimation[i] = anim
-                func = "AnimationManager.{}()".format(self.listAnimation[i])
-                exec(func)
-            i += 1
+                if anim == "flash":
+                    self.gestionSlide.camera.game.actuators[obj.name].to_property = obj.name + "-flash"
+                    obj.name += "-flash"
+                    AnimationManager.animation(anim)
 
+                if anim == "startMotion":
+                    self.startMotion = obj.location.copy()
 
-    def startTransition(self):
+                if anim == "endMotion":
+                    self.endMotion = obj.location.copy()
 
-        """TODO"""
+                if anim == "motion":
 
-        self.transitionDone = True
+                    minStartMotionX = "% 5.2f" % self.startMotion[0]
+                    minStartMotionY = "% 5.2f" % self.startMotion[1]
+                    minStartMotionZ = "% 5.2f" % self.startMotion[2]
 
-    def startAction(self):
+                    self.gestionSlide.camera.game.actuators[obj.name].to_property = obj.name + "-motion({},{},{})".format(minStartMotionX, minStartMotionY, minStartMotionZ)
+                    obj.name += "-motion({},{},{})".format(minStartMotionX, minStartMotionY, minStartMotionZ)
+                    AnimationManager.animation(anim, self.startMotion, self.endMotion)
 
-        """TODO"""
+                if anim == "cancelMotion":
+                    self.startMotion = [0.0, 0.0, 0.0]
+                    self.endMotion = [0.0, 0.0, 0.0]
 
-        self.actionDone = True
-
-    def next(self):
-        if len(self.listObjects) != 0:
-            self.startTransition()
-        elif self.pos == len(self.listObjects) - 1:
-            if self.transitionDone and self.actionDone:
-                print('NEXT')
-        #else:
-
+    def removeAnimation(self, anim):
+        print('Etape 3', context.active_object.name)
+        for obj in self.listObjects:
+            if obj == context.active_object:
+                print('Etape 4')
+                if anim == "delFlash":
+                    print('Etape 5')
+                    obj.name = obj.name[0:8]
+                    self.gestionSlide.camera.game.actuators[obj.name].to_property = obj.name
+                    AnimationManager.animation(anim)
+                if anim == "delMotion":
+                    obj.name = obj.name[0:8]
+                    self.gestionSlide.camera.game.actuators[obj.name].to_property = obj.name
+                    AnimationManager.animation(anim)
 
     def __del__(self):
         ops.object.select_all(action='DESELECT')
@@ -82,14 +129,25 @@ class SlidesManager(Singloton):
 
                 sensors = context.object.game.sensors
                 controllers = context.object.game.controllers
+                actuators = context.object.game.actuators
                 ops.logic.sensor_add(type='ALWAYS')
                 sensor = sensors[-1]
                 sensor.use_pulse_true_level = True
                 ops.logic.controller_add(type='PYTHON')
                 controller = controllers[-1]
-                controller.mode = 'SCRIPT'
-                controller.text = data.texts["run.py"]
-                sensor.link(controller)
+                controller.mode = 'MODULE'
+                controller.module = "run.next"
+                ops.logic.actuator_add(type='MOTION')
+                actuator1 = actuators[-1]
+                actuator1.name = "transition"
+                actuator1.offset_location[0] = 0.2
+                ops.logic.actuator_add(type='CONSTRAINT')
+                actuator2 = actuators[-1]
+                actuator2.name = "limitTransition"
+                actuator2.limit = 'LOCX'
+                actuator2.limit_max = 0
+                controller.link(sensor, actuator1)
+                controller.link(None, actuator2)
 
             else:
                 if self.posActiveSlide + 1 == self.nbSlides:
@@ -107,13 +165,12 @@ class SlidesManager(Singloton):
                         self.listSlides[i].cameraPos[0] += 20
                         for obj in self.listSlides[i].listObjects:
                             obj.location.x += 20
-                            obj.name = "Object_Slide-{}".format(self.posActiveSlide + 2)
+                            self.listSlides[i].renameObject(obj, int(obj.name[5]) + 1, None)
 
             context.scene.prop_nb_slides['Active_Slide'] = self.posActiveSlide + 2
             self.posActiveSlide += 1
 
             self.addNbSlide()
-
 
         def getNbSlides(self):
             return self.nbSlides
@@ -150,7 +207,7 @@ class SlidesManager(Singloton):
                     self.listSlides[i].cameraPos[0] -= 20
                     for obj in self.listSlides[i].listObjects:
                         obj.location.x -= 20
-                        obj.name = "Object_Slide-{}".format(self.posActiveSlide + 1)
+                        self.listSlides[i].renameObject(obj, int(obj.name[5]) - 1, None)
 
                 del self.listSlides[self.posActiveSlide]
 
