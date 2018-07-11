a5778ede0dcb4ed968a5decae45b2102207aba7f
Killian <killian.perche@outlook.fr>
2018-07-11 21:34:44 +0200

    Removing unused files and creating a singleton decorator

--- apis/pypredef/BPL/Debug.py
+++
@@ -1,8 +0,0 @@
-def Debug(context, *args, **kwargs):
-    msg = "Debug: " + context
-    for arg in args:
-        msg += "\n\t" + str(arg)
-    for key in kwargs:
-        msg += "\n\t" + key + ": " + str(kwargs[key])
-
-    print(msg)
